5ccbcb376dc35711900f597d1110406ace8ae79a
sebastienbw <sebastienbw@gmail.com>
2015-03-24 13:45:13 -0700

    display image after taking picture

--- js/base.js
+++ js/base.js
@@ -13,9 +13,9 @@ scavengrApp.config(function($routeProvider) {
         })
 
         // route for the about page
-        .when('/home', {
-            templateUrl : 'pages/home.html',
-            controller  : 'homeController'
+        .when('/camera', {
+            templateUrl : 'pages/camera.html',
+            controller  : 'cameraController'
         })
 });
 
@@ -41,16 +41,16 @@ scavengrApp.controller('appController', function($scope) {
     }
 });
 
-scavengrApp.controller('homeController', function($scope) {
-    $scope.message = "Honey, I'm Home!";
-});
-
-(function () {
-    var takePicture = document.querySelector("#take-picture"),
+scavengrApp.controller('cameraController', function($scope) {
+    $scope.message = "Camera";
+    $scope.takePicture = function () {
+        console.log("pucture!")
+        var takePicture = document.querySelector("#take-picture"),
         showPicture = document.querySelector("#show-picture");
-
+        console.log("page loaded");
     if (takePicture && showPicture) {
         // Set events
+        console.log("found camera stuff");
         takePicture.onchange = function (event) {
             // Get a reference to the taken picture or chosen file
             var files = event.target.files,
@@ -66,6 +66,7 @@ scavengrApp.controller('homeController', function($scope) {
 
                     // Set img src to ObjectURL
                     showPicture.src = imgURL;
+                    
 
                     // Revoke ObjectURL
                     URL.revokeObjectURL(imgURL);
@@ -90,4 +91,54 @@ scavengrApp.controller('homeController', function($scope) {
             }
         };
     }
-})();
+    };
+});
+
+// (function () {
+//     var takePicture = document.querySelector("#take-picture"),
+//         showPicture = document.querySelector("#show-picture");
+//         console.log("page loaded");
+//     if (takePicture && showPicture) {
+//         // Set events
+//         console.log("found camera stuff");
+//         takePicture.onchange = function (event) {
+//             // Get a reference to the taken picture or chosen file
+//             var files = event.target.files,
+//                 file;
+//             if (files && files.length > 0) {
+//                 file = files[0];
+//                 try {
+//                     // Get window.URL object
+//                     var URL = window.URL || window.webkitURL;
+
+//                     // Create ObjectURL
+//                     var imgURL = URL.createObjectURL(file);
+
+//                     // Set img src to ObjectURL
+//                     showPicture.src = imgURL;
+                    
+
+//                     // Revoke ObjectURL
+//                     URL.revokeObjectURL(imgURL);
+//                 }
+//                 catch (e) {
+//                     try {
+//                         // Fallback if createObjectURL is not supported
+//                         var fileReader = new FileReader();
+//                         fileReader.onload = function (event) {
+//                             showPicture.src = event.target.result;
+//                         };
+//                         fileReader.readAsDataURL(file);
+//                     }
+//                     catch (e) {
+//                         //
+//                         var error = document.querySelector("#error");
+//                         if (error) {
+//                             error.innerHTML = "Neither createObjectURL or FileReader are supported";
+//                         }
+//                     }
+//                 }
+//             }
+//         };
+//     }
+// })();
